5e2469ab05e84e6c5b573ebc50c6784e178063e0
BehemothDi <dmitriiprozherin@mail.ru>
2023-04-03 19:13:16 +0300

    Добавлен экран главного меню

--- lib/ui/main_menu_screen.dart
+++ lib/ui/main_menu_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class MainMenuScreen extends StatelessWidget {
@@ -5,6 +7,81 @@ class MainMenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game Menu'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 400,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text('Начать игру'),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: 400,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text('Выбор уровня'),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: 400,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text('Выбор сложности'),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: 400,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text('Настройки'),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            SizedBox(
+              width: 400,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () => exit(0),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple,
+                ),
+                child: const Text('Выход'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
   }
 }
